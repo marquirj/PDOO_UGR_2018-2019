5c20a81370f5697cac8f79eff3ad7b030a6b10ef
marquirj <juanmartinquiros@correo.ugr.es>
2018-10-28 18:44:58 +0100

    Modulo EstadoJuego

---
+++ P3/QytetetRuby/lib/estado_juego.rb
@@ -0,0 +1,15 @@
+#ENCODING: UTF-8
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+module QytetetRuby
+module EstadoJuego
+  JA_CONSORPRESA= JA_Con_Sorpresa
+  ALGUNJUGADORENBANCARROTA= :Algun_jugador_en_bancarrota
+  JA_PUEDECOMPRAROGESTIONAR= :Ja_Puede_comprar_o_gestionar
+  JA_PUEDEGESTIONAR= :JA_Puedes_gestoonar
+  JA_PREPARADO= :JA_Preparado
+  JA_ENCARCELADO= :JA_Encarcelado
+  JA_ENCARCELADOCONOPCIONDELIBERTAD= :JA_encarcelado_con_opcion_de_libertad
+end
+end
